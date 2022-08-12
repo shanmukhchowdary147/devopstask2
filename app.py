5c4cdc3b0e4665aa047d93061bf235e42bc546e8
Shanmukh Chowdary <57340285+shanmukhchowdary147@users.noreply.github.com>
2022-08-12 16:33:44 +0530

    up

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world!"
+    return "Hello world!!!"
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int("3001"))
